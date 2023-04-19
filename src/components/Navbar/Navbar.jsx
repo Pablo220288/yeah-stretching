3e6fb57d25c2a585b122f13914605c6294241f4a
Pab Her <peh_tj@hotmail.com>
2023-04-19 08:58:20 -0300

    Img & Navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,12 +1,20 @@
-import { NavLink } from 'react-router-dom'
-
+import { NavLink } from "react-router-dom";
+import logo from "../../assets/LOGO WEB.svg"
 
 export const Navbar = () => {
-    return (
-        <nav className='navbar__container'>
-            <NavLink to="/about">Sobre Mi</NavLink>
-            <NavLink to="/home">Yeah Stretching</NavLink>
-            <NavLink to="/contact">Conacto!</NavLink>
-        </nav>
-    )
-}
+  return (
+    <nav className="navbar__container">
+      <div className="navbar__logo">
+        <NavLink to="/home">
+          <img src={logo} alt="yeha stretching logo" />
+        </NavLink>
+      </div>
+      <div className="navbar__link__container">
+        <NavLink to="/home">Home</NavLink>
+        <NavLink to="/about">Sobre Mi</NavLink>
+        <NavLink to="/service">Servicios</NavLink>
+        <NavLink to="/contact">Conacto</NavLink>
+      </div>
+    </nav>
+  );
+};
--- src/components/index.js
+++ src/components/index.js
@@ -4,3 +4,4 @@ export * from './Footer';
 export * from './ContactForm';
 export * from './Home';
 export * from './Navbar';
+export * from './Service';
